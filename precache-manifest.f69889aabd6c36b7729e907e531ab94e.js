5330ffc25a60b5357d99d45ac95d1c975d609133
Felipe Gabriel Arend <fga.felipe@gmail.com>
2019-04-20 21:29:15 -0300

    Updates

---
+++ precache-manifest.f69889aabd6c36b7729e907e531ab94e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da832b12a0c136ee3c32",
+    "url": "/react-flip-card/static/js/runtime~main.7ddfda00.js"
+  },
+  {
+    "revision": "466b5aeeb618bff8a310",
+    "url": "/react-flip-card/static/js/main.37964573.chunk.js"
+  },
+  {
+    "revision": "c0568eb1b09c8d6ba9b2",
+    "url": "/react-flip-card/static/js/2.c084affd.chunk.js"
+  },
+  {
+    "revision": "466b5aeeb618bff8a310",
+    "url": "/react-flip-card/static/css/main.e9e1aceb.chunk.css"
+  },
+  {
+    "revision": "0fa51a267586ced0ec0ec48f1f785e09",
+    "url": "/react-flip-card/index.html"
+  }
+];
